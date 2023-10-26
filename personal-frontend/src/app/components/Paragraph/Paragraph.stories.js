988aa0dadf73c3511412372c2d2b7fdfa2c76552
nathan harris <nharrisanalyst@gmail.com>
2023-10-26 10:05:18 -0700

    first commit

---
+++ personal-frontend/src/app/components/Paragraph/Paragraph.stories.js
@@ -0,0 +1,15 @@
+import { Paragraph } from './';
+import textData from './mainText.json';
+
+export default {
+    title:'Molecules/Paragraph',
+    component: Paragraph,
+    tags: ['autodocs'],
+}
+
+
+export const Primary ={
+    args:{
+        textData
+    }
+}
---
+++ personal-frontend/src/app/components/text/index.js
@@ -0,0 +1,50 @@
+import PropTypes from 'prop-types';
+import './text.css';
+
+
+export const Text =({ 
+     color= "black", 
+     children, 
+     strong = false,
+     size = "1.5em", 
+     lineBreak = false 
+    }) =>{
+    const styles = {
+        color,
+        fontSize:size
+    }
+    if(strong){
+        return (
+        <>
+        <strong>   
+        <span className="customText" style = {styles}>
+        {children}
+        </span>
+        </strong>
+        {lineBreak?<br/>:""}
+        </>
+        )
+    }
+   return (
+        <>
+        <span className="customText" style = {styles}>
+        {children}
+        </span>
+        {lineBreak?<br/>:""}
+        </>
+
+   )
+}
+
+Text.propTypes = {
+    color:  PropTypes.string,
+    children: PropTypes.string,
+    strong: PropTypes.bool,
+    size: PropTypes.string,
+    linebreak: PropTypes.bool,
+
+}
+
+
+
+
