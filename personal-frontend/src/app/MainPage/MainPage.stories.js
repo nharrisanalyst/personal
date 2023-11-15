98ee120e476af10d51e3e89ae3077ddf19ec4100
nathan harris <nharrisanalyst@gmail.com>
2023-11-15 14:12:15 -0800

    started <MainPage />

---
+++ personal-frontend/src/app/MainPage/MainPage.stories.js
@@ -0,0 +1,12 @@
+import { MainPage } from './';
+
+export default{
+    title:'Page/Main',
+    component:MainPage,
+    tags: ['autodocs'],
+}
+
+
+export const Primary ={
+    args:{},
+}
---
+++ personal-frontend/src/app/MainPage/index.js
@@ -0,0 +1,10 @@
+import { Header } from '../components/Header';
+
+import "./mainPage.css";
+
+
+export const MainPage =()=>(
+    <>
+        <Header />
+    </>
+)
