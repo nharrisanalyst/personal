baed07b899a22a5510b80dd7f2491b218dde50f0
nathan harris <nharrisanalyst@gmail.com>
2023-11-15 14:54:42 -0800

    aligned resume link and styled maintop

--- personal-frontend/src/app/MainPage/index.js
+++ personal-frontend/src/app/MainPage/index.js
@@ -1,4 +1,10 @@
 import { Header } from '../components/Header';
+import { Paragraph } from '../components/Paragraph';
+import textDataMain from '../components/Paragraph/mainText.json';
+import midParagraphData from '../components/Paragraph/midParagraphText.json';
+import { ResumeLink } from '../components/ResumeLink';
+import { WorkSamplesLayout } from '../layouts/WorkSamplesLayout';
+import workData from "../layouts/WorkSamplesLayout/workData.json";
 
 import "./mainPage.css";
 
@@ -6,5 +12,21 @@ import "./mainPage.css";
 export const MainPage =()=>(
     <>
         <Header />
+        <div className="main-paig-main">
+            <div className ='main-top-section'>
+                <div className='main-text-top'>
+                    <Paragraph textData={textDataMain} />
+                </div>
+                <div className='main-resume-link'>
+                    <ResumeLink />
+                </div>
+            </div>
+            <div className='main-mid-section'>
+                <Paragraph textData={midParagraphData} />
+            </div>
+            <div className='work-sample-section'>
+                <WorkSamplesLayout sampleWorkData={workData} />
+            </div>
+        </div>
     </>
 )
