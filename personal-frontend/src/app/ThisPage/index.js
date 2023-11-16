aace6971f668918121fabd2074d1a5ed8a7be600
nathan harris <nharrisanalyst@gmail.com>
2023-11-16 12:06:22 -0800

    started ThisPage

---
+++ personal-frontend/src/app/ThisPage/index.js
@@ -0,0 +1,14 @@
+import "./thisPage.css";
+import { Header } from '../components/Header';
+
+
+export const ThisPage =()=>(
+    <>
+    <Header />
+    <div className='this-page-main'>
+        <div className='this-page-designs'>
+            My Personal Page Resource:
+        </div>
+    </div>
+    </>
+)
